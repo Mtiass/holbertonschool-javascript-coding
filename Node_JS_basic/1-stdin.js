00c065f67744fade46a8f3323e08f14e75870247
Mtiass <MDLSproff@gmail.com>
2024-05-09 22:32:54 -0300

    Task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+// Program named 1-stdin.js that will be executed through command line.
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+});

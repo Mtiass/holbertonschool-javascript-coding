970fe91eeba9cb29a09c911e2656bc6a3663e081
Mtiass <MDLSproff@gmail.com>
2024-07-04 22:18:12 +0000

    task 1 fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -9,7 +9,7 @@ const rl = readline.createInterface({
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
-  console.log(`Your name is: ${input}\r`);
+  console.log(`Your name is: ${input}\n`);
 });
 
 rl.on('close', () => {

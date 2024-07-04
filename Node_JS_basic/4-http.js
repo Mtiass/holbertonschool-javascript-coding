1cd13cc4ca60e9d7fc2b0c1f7733972d9c797c58
Mtiass <MDLSproff@gmail.com>
2024-07-04 23:56:38 +0000

    T4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,13 +2,13 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  
+
   res.end('Hello Holberton School!\n');
 });
 
 const PORT = 1245;
 app.listen(PORT, () => {
-  console.log(`Server is running and listening on port ${PORT}`);
+  console.log(`Server is running and listening on http://localhost:${PORT}`);
 });
 
 module.exports = app;

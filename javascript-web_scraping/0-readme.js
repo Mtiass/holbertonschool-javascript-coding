bb85f446d26dfe888612084a497eb4c7654a3b99
Mtiass <MDLSproff@gmail.com>
2024-07-04 01:45:15 +0000

    Beginning

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,18 +2,12 @@
 // Script to read and print the content of a file
 const fs = require('fs');
 
-function readFile(filePath) {
-try {
-	const content = fs.readFileSync(filePath, 'utf-8');
-	console.log(content);
-} catch (error) {
-	console.error(error);
-}
+const filePath = process.argv[2];
 
-}
-if (process.argv.length > 2) {
-	const filePath = process.argv[2];
-	readFile(filePath);
-} else {
-	console.error('Please provide a file path as an argument.');
-}
+fs.readFile(filePath, 'utf-8', (err, data) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	console.log(data);
+});

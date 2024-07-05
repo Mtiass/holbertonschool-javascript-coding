795ef415b81eafab28745e45d743ef0e514bbe46
Mtiass <MDLSproff@gmail.com>
2024-07-05 00:38:26 +0000

    t2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,19 +2,21 @@
 // Script that display the status code of a GET request.
 const request = require('request');
 
+const url = process.argv[2];
+
+function getStatus(url) {
+  request.get(url, (error, response) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(`code: ${response.statusCode}`);
+  });
+}
+
 if (process.argv.length !== 3) {
   console.error('Usage: ./2-statuscode.js <URL>');
   process.exit(1);
 }
 
-const url = process.argv[2];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  
-  console.log(`code: ${response.statusCode}`);
-});
-
+getStatus(url);

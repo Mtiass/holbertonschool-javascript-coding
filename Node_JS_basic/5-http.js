e51697d09d7f791f921bc3e504f0361ec1e6fc82
Mtiass <MDLSproff@gmail.com>
2024-07-05 00:07:00 +0000

    T5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,23 +1,24 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const { countStudents } = require('./3-read_file_async');
 
-const filePath = process.argv[2];
+const databasePath = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.setHeader('Content-Type', 'text/plain');
 
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    countStudents(filePath)
-      .then(() => {
+    countStudents(databasePath)
+      .then((data) => {
+        res.write('This is the list of our students\n');
+        data.forEach((line) => {
+          res.write(`${line}\n`);
+        });
+        res.end();
       })
       .catch((error) => {
-        console.error(error.message);
-        res.statusCode = 500;
-        res.end('Internal Server Error');
+        res.end(`This is the list of our students\n${error.message}`);
       });
   } else {
     res.statusCode = 404;
@@ -26,8 +27,9 @@ const app = http.createServer((req, res) => {
 });
 
 const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is running and listening on http://localhost:${PORT}`);
+const HOSTNAME = 'localhost';
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is running and listening on http://${HOSTNAME}:${PORT}`);
 });
 
 module.exports = app;

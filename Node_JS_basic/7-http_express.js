17e43099320401d031c4eb28658fe1d1d312590d
Mtiass <MDLSproff@gmail.com>
2024-07-05 00:23:23 +0000

    t7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,56 +1,31 @@
 const express = require('express');
-const fs = require('fs').promises;
-const csvParse = require('csv-parse');
+const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const records = await csvParse(data, { columns: true, skip_empty_lines: true });
-
-    let totalStudents = 0;
-    let fields = {};
-
-    records.forEach(record => {
-      if (record.firstName && record.field) {
-        totalStudents++;
-        if (!fields[record.field]) {
-          fields[record.field] = [];
-        }
-        fields[record.field].push(record.firstName);
-      }
-    });
-
-    let results = [`Number of students: ${totalStudents}`];
-    Object.keys(fields).forEach(field => {
-      results.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-    });
-
-    return results.join('\n');
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-}
+const databasePath = 'database.csv';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.type('text').send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  const databasePath = req.query.db; 
-
-  try {
-    const studentInfo = await countStudents(databasePath);
-    res.type('text').send(`This is the list of our students\n${studentInfo}`);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+app.get('/students', (req, res) => {
+  fs.access(databasePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      res.send('This is the list of our students\nDatabase not found');
+    } else {
+      countStudents(databasePath)
+        .then((data) => {
+          res.send(`This is the list of our students\n${data}`);
+        })
+        .catch((error) => {
+          res.send(`This is the list of our students\n${error.message}`);
+        });
+    }
+  });
 });
 
-const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is running and listening on port ${PORT}`);
-});
+app.listen(1245);
 
 module.exports = app;

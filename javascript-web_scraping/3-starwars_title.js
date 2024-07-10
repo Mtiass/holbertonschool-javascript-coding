db2ea1ac7e73e5b32d9630ee1ffe7c349ade430b
Mtiass <MDLSproff@gmail.com>
2024-07-10 00:35:44 +0000

    Task 3 fix

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,10 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 request(url, function (error, response, body) {
-	if (error) {
-		console.error(error);
-	} else {
-		const movie = JSON.parse(body);
-		console.log(movie.title);
-	}
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
 });

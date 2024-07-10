3eaf41214c473206fcca7966cb83ef5366711508
Mtiass <MDLSproff@gmail.com>
2024-07-10 00:20:23 +0000

    Task 3 fix

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 // Script that prints the title of a movie where the ep n matches a given int.
 const request = require('request');
 const movieId = process.argv[2];
-const url = https://swapi-api.hbtn.io/api/films/${movieId};
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 request(url, function (error, response, body) {
 	if (error) {
 		console.error(error);
